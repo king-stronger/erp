5356fa30c8146336b5fde73b7731823712be807f
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-05-10 16:07:00 +0000

    feat: initialize Express server with EJS and static file handling
    - Serve static files from /public directory
    - Set up express app with middleware for form and json data
    - Configure EJS as the view engine
    - Use __dirname logic compatible with ES modules

---
+++ app.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import express from 'express';
+import { fileURLToPath} from 'url';
+
+// Declare the constant variables
+const PORT = process.env.PORT;
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Init the express app
+const app = express();
+
+// Set a middleware to handle static files
+app.use(express.static(path.join(dirname, "public")));
+
+// Set a middleware to handle form and json data
+app.use(express.urlencoded({ extended: true }));
+
+// Set the view engine to ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(dirname, "views"));
+
+// Make the app listen on a specified port
+app.listen(PORT, () => {
+    console.log(`Server listening on port : ${PORT}`);
+})
