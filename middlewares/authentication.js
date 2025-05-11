013fdddd3463779ce5d236ea7e6dd2cdb9450670
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-05-11 11:43:00 +0000

    feat: add the isNotAuthenticated middleware function

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -4,6 +4,13 @@ async function isAuthenticated(req, res, next){
     return next();
 }
 
+async function isNotAuthenticated(req, res, next){
+    if(!req.user) res.json({ message: "Must be authenticated" });
+
+    return next();
+}
+
 export {
-    isAuthenticated
+    isAuthenticated,
+    isNotAuthenticated
 }
