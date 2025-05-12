3999aaef0fc9ecba44d1edb66c547b16eb535ac2
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-05-12 21:01:00 +0000

    feat: import the payment router to the main router

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth.js";
+import paymentRouter from "./payments.js";
 import categoryRouter from "./categories.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
+router.use("/payments", paymentRouter);
 router.use("/categories", categoryRouter);
 
 export default router;
