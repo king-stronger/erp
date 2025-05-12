db6536c88e02d79ecdba2b4ead48ccd9c3f5573c
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-05-12 21:15:00 +0000

    fix: the logic for the user being authenticated or not was reversed

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,11 +1,11 @@
 async function isAuthenticated(req, res, next){
-    if(req.user) res.json({ message: "Not authenticated" });
+    if(!req.user) return res.json({ message: "Must be authenticated" });
 
     return next();
 }
 
 async function isNotAuthenticated(req, res, next){
-    if(!req.user) res.json({ message: "Must be authenticated" });
+    if(req.user) return res.json({ message: "You're already authenticated !" });
 
     return next();
 }
