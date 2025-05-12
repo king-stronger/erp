6bca333da349981407d5f3b1a829942cd9970175
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-05-12 21:25:00 +0000

    feat: add the isAuthenticated middleware
    Add the isAuthenticated middleware to the payments and categories routes

--- routes/router.js
+++ routes/router.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import authRouter from "./auth.js";
 import paymentRouter from "./payments.js";
 import categoryRouter from "./categories.js";
+import { isAuthenticated } from "../middlewares/authentication.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
-router.use("/payments", paymentRouter);
-router.use("/categories", categoryRouter);
+router.use("/payments", isAuthenticated, paymentRouter);
+router.use("/categories", isAuthenticated, categoryRouter);
 
 export default router;
