13afb4cdbba85048d371929ae97ebd69b0ff2c92
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-05-12 20:40:00 +0000

    feat: import the category router to the main router

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./auth.js";
+import categoryRouter from "./categories.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
+router.use("/categories", categoryRouter);
 
 export default router;
