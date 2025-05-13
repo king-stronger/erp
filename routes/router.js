b22d8f0455c1e10796d5f98aea8e629d38c8466a
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-05-13 13:49:00 +0000

    feat: import the expense router in the main router

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authRouter from "./auth.js";
 import paymentRouter from "./payments.js";
 import revenueRouter from "./revenues.js";
+import expenseRouter from "./expenses.js";
 import categoryRouter from "./categories.js";
 import { isAuthenticated } from "../middlewares/authentication.js";
 
@@ -10,6 +11,7 @@ const router = Router();
 router.use("/auth", authRouter);
 router.use("/payments", isAuthenticated, paymentRouter);
 router.use("/revenues", isAuthenticated, revenueRouter);
+router.use("/expenses", isAuthenticated, expenseRouter);
 router.use("/categories", isAuthenticated, categoryRouter);
 
 export default router;
