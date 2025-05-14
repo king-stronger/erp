9b3744ddbaaa7914c6433a200183533f8c25bd1b
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-05-14 16:14:00 +0000

    feat: use validate function in the relevant routes
    import the categorySchema and use the validate function in the store and update route

--- routes/categories.js
+++ routes/categories.js
@@ -7,14 +7,19 @@ import {
     storeCategory,
     updateCategory
 } from "../controllers/category.js";
+import { 
+    createCategorySchema,
+    updateCategorySchema
+} from "../validations/category.js";
+import { validate } from "../utils/validation.js";
 
 const categoryRouter = Router();
 
 categoryRouter.get("/", getAllCategories);
 categoryRouter.get("/new", createCategory);
 categoryRouter.get("/:id/edit", editCategory);
-categoryRouter.post("/", storeCategory);
-categoryRouter.put("/:id", updateCategory);
 categoryRouter.delete("/:id", deleteCategory);
+categoryRouter.post("/", validate(createCategorySchema), storeCategory);
+categoryRouter.put("/:id", validate(updateCategorySchema), updateCategory);
 
 export default categoryRouter;
