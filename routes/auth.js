6c11a78832129a5d4a8b4a6a29ffd0a5e1154600
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-05-15 20:21:00 +0000

    refactor: import the validate middleware
    Import the validate middleware and the register schema

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,8 @@ import {
     isAuthenticated,
     isNotAuthenticated
 } from "../middlewares/authentication.js";
+import { validate } from "../middlewares/validation.js";
+import { registerSchema } from "../validations/auth.js";
 
 const authRouter = Router();
 
@@ -15,6 +17,6 @@ authRouter.get("/login", isNotAuthenticated, login);
 authRouter.post("/login", isNotAuthenticated, login);
 authRouter.post("/logout", isAuthenticated, logout);
 authRouter.get("/register", isNotAuthenticated, register);
-authRouter.post("/register", isNotAuthenticated, register);
+authRouter.post("/register", isNotAuthenticated, validate(registerSchema), register);
 
 export default authRouter;
